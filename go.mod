d0c28079da4f2e1f74feb5a7746195fe03ca8c04
Jéssica Souza <jessica+pgp-signed-commit@souza.cloud>
2022-09-20 08:22:32 -0300

    implement test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jessicamosouza/GoWithTests
+
+go 1.19
---
+++ hello_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+
+// It needs to be in a file with a name like xxx_test.go
+// The test function must start with the word Test
+// The test function takes one argument only t *testing.T
+// In order to use the *testing.T type, you need to import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to 'world", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	{
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+}
