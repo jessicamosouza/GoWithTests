66d3f7aaa01aa63619903e7d5b874bd226e02de6
Jéssica Souza <jessica+pgp-signed-commit@souza.cloud>
2022-09-29 15:35:21 -0300

    add interface

--- fundamentals/structsMethodInterfaces/shapes_test.go
+++ fundamentals/structsMethodInterfaces/shapes_test.go
@@ -14,26 +14,28 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
-func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		got := rectangle.Area()
-		want := 72.0
+type Shape interface {
+	Area() float64
+}
 
+func TestArea(t *testing.T) {
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
-			t.Errorf("got %.2f wanr %.2f", got, want)
+			t.Errorf("got %g want %g", got, want)
 		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			// %g will print a more precise decimal number in the error message
-			t.Errorf("got %g want %g", got, want)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
 
 }
