b6fb82ed472181d3437a0a3a8f394b5d357fb0f2
Jéssica Souza <jessica+pgp-signed-commit@souza.cloud>
2022-09-30 07:27:04 -0300

    add withdraw and some fix

--- fundamentals/pointersAndErrors/wallet.go
+++ fundamentals/pointersAndErrors/wallet.go
@@ -1,15 +1,30 @@
 package pointersAndErrors
 
+import "fmt"
+
+type Bitcoin int
 type Wallet struct {
-	balance int
+	balance Bitcoin
+}
+
+type Stringer interface {
+	String() string
 }
 
 // take a pointer to that wallet so that we can change the original values within it
-//(w *Wallet) = struct pointers
-func (w *Wallet) Deposit(amount int) {
+// (w *Wallet) = struct pointers
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
